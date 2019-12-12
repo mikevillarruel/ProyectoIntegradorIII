b23b953165e4064a46af95759da9673d29ef8742
Michael Villarruel <alejovillarruel@gmail.com>
2019-12-11 21:54:21 -0500

    Fecha Ingreso

--- proyecto/proyecto/Vista/RegistroComprador.aspx.cs
+++ proyecto/proyecto/Vista/RegistroComprador.aspx.cs
@@ -18,7 +18,8 @@ namespace proyecto.Vista.webfonts
 
         protected void btnRegistrar_Click(object sender, EventArgs e)
         {
-            Comprador comprador = new Comprador(0, nombre.Value, apellido.Value, cedula.Value, usuario.Value, contrasenia.Value, ciudad.Value, 1, email.Value, direccion.Value, telefono.Value, new DateTime(2017, 2, 23));
+            DateTime aDate = DateTime.ParseExact(fechaDeNacimiento.Value, "yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture);
+            Comprador comprador = new Comprador(0, nombre.Value, apellido.Value, cedula.Value, usuario.Value, contrasenia.Value, ciudad.Value, 1, email.Value, direccion.Value, telefono.Value, aDate);
             Servicio servicio = new Servicio();
             string script;
             if (servicio.insertComprador(comprador))
