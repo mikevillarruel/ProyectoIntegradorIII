887e64de270325dd4dd1871372ffaccd99acad71
kdquistanchala <51094604+kdquistanchala@users.noreply.github.com>
2020-01-17 00:33:24 -0500

    nuevo1.0

---
+++ e-commerce/e-commerce/e-commerce/Controllers/Servicio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using e_commerce.Models;
+
+namespace e_commerce.Controllers
+{
+    public class Servicio
+    {
+        public String selectPrueba()
+        {
+            Operaciones operaciones = new Operaciones();
+            String prueba;
+            prueba = operaciones.selectPrueba();
+            return prueba;
+        }
+    }
+}
---
+++ e-commerce/e-commerce/e-commerce/Models/Operaciones.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Oracle.DataAccess.Client;
+using System.Data;
+
+namespace e_commerce.Models
+{
+    public class Operaciones
+    {
+        public String selectPrueba()
+        {
+            //ADMIN 1
+            //COMPRADOR 2
+            String prueba="";
+            try
+            {
+                OracleConnection conn = Conexion.getInstancia().getConexion();
+                OracleCommand oracleCommand = conn.CreateCommand();
+
+                oracleCommand.CommandText = "SELECT PRO_NOMBRE FROM VNT_PRODUCTO";
+                oracleCommand.CommandType = CommandType.Text;
+                OracleDataReader odr = oracleCommand.ExecuteReader();
+
+                if (odr.Read())
+                {
+                    prueba = odr.GetString(0);
+                    return prueba;
+                }
+            }
+            catch
+            {
+                return prueba;
+            }
+            return prueba;
+        }
+    }
+}
